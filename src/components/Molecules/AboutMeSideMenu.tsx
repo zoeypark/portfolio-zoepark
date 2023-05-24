9b3e18c146b6fb83c811e1afa78a091e40c37c64
Zoe Park <mary990t@gmail.com>
2023-05-24 21:57:47 +0900

    chore: Create AboutMeSideMenu

--- src/components/Molecules/AboutMeSideMenu.tsx
+++ src/components/Molecules/AboutMeSideMenu.tsx
@@ -1,5 +1,28 @@
+import styled from "styled-components"
+
+const StyledUl = styled.ul`
+  position: sticky;
+  top: 0rem;
+  right: 15%;
+  z-index: 100rem;
+`
+
+const StyledLi = styled.li`
+  :hover {
+    text-decoration: underline;
+  }
+`
+
 const AboutMeSideMenu = () => {
   return (
-    <></>
+    <>
+      <StyledUl>
+        <StyledLi>about me</StyledLi>
+        <StyledLi>education</StyledLi>
+        <StyledLi>experience</StyledLi>
+      </StyledUl>
+    </>
   )
 }
+
+export default AboutMeSideMenu;
--- src/components/Organisms/AboutMeCardScroll.tsx
+++ src/components/Organisms/AboutMeCardScroll.tsx
@@ -4,7 +4,6 @@ import AboutMeCard from "../Molecules/AboutMeCard"
 const ScrollWrapper = styled.div`
   width: 100%;
   display: flex;
-  padding-top: 8rem;
   justify-content: center;
   align-items: center;
 ` 
@@ -15,6 +14,12 @@ const AboutMeCardScroll = () => {
       <ScrollWrapper>
         <ul>
           {/*map으로 li요소 돌려서 컴포넌트 여러개 표시하기*/}
+          <li>
+            <AboutMeCard />
+          </li>
+          <li>
+            <AboutMeCard />
+          </li>
           <li>
             <AboutMeCard />
           </li>
