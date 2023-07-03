1779d3d714bb21449d38159920bf3f1228cf3043
Zoe Park <mary990t@gmail.com>
2023-07-03 23:50:53 +0900

    feat: Create Molecules

---
+++ src/assets/dummydata/dummydata.js
@@ -0,0 +1,23 @@
+const cards = [
+  {
+    id: 1,
+    img: "",
+    title: "about me",
+    desc: "1996년생",
+    content: "도전, 끈기, 호기심! 현재 서울에 있습니다. typescript를 공부 중입니다."
+  },
+  {
+    id: 2,
+    img: "",
+    title: "experience",
+    desc: "",
+    content: "컴퓨팅사고와 데이터 분석 기초 강의 수강 / 코드 스테이츠 프론트엔드 6개월 코스 수료"
+  },
+  {
+    id: 3,
+    img: "",
+    title: "projects",
+    desc: "",
+    content: "Azit: 취미 공유 모임 서비스 앱 / Todo Today: 회원 가입과 로그인이 가능한 투두 리스트 앱"
+  }
+]
--- src/components/Molecules/AboutMeMenu.tsx
+++ src/components/Molecules/AboutMeMenu.tsx
@@ -1,26 +1,15 @@
 import { useRef } from "react";
 import styled from "styled-components"
 
-const StyledSideUl = styled.ul`
-  position: fixed;
-  top: 11rem;
-  right: 20%;
-  z-index: 100rem;
-`
-
-const StyledLi = styled.li`
-  > button {
-    all: unset;
-    :hover {
-    cursor: pointer;
-    text-decoration: underline;
-  }
-  }
-`
-
 const StyledTopUl = styled.ul`
   display: flex;
-  gap: 1rem;
+  justify-content: center;
+  gap: 4rem;
+  padding-bottom: 1rem;
+  padding-top: 2rem;
+  background-color: #EFEBE6;
+  position:sticky;
+  top:3rem;
   > li {
     > button {
       all: unset;
@@ -32,28 +21,6 @@ const StyledTopUl = styled.ul`
   }
 `
 
-export const AboutMeSideMenu: React.FC = () => {
-
-    const sectionRefs = {
-      aboutme: useRef<HTMLLIElement>(null),
-      education: useRef<HTMLLIElement>(null),
-      experience: useRef<HTMLLIElement>(null),
-    };
-
-    const scrollToSection = (section: keyof typeof sectionRefs) => {
-      sectionRefs[section].current?.scrollIntoView({ behavior: 'smooth' })
-    }
-  
-  return (
-    <>
-      <StyledSideUl>
-        <StyledLi onClick={() => scrollToSection('aboutme')}><button>about me</button></StyledLi>
-        <StyledLi onClick={() => scrollToSection('education')}><button>education</button></StyledLi>
-        <StyledLi onClick={() => scrollToSection('experience')}><button>experience</button></StyledLi>
-      </StyledSideUl>
-    </>
-  )
-}
 
 export const AboutMeTopMenu: React.FC = () => {
 
@@ -71,8 +38,8 @@ return (
   <>
     <StyledTopUl>
       <li onClick={() => scrollToSection('aboutme')}><button>about me</button></li>
-      <li onClick={() => scrollToSection('education')}><button>education</button></li>
       <li onClick={() => scrollToSection('experience')}><button>experience</button></li>
+      <li onClick={() => scrollToSection('experience')}><button>projects</button></li>
     </StyledTopUl>
   </>
 )
