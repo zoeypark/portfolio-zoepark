430c49461d4a3d059cdc80e16a851228921eff4e
Zoe Park <mary990t@gmail.com>
2023-07-08 20:56:22 +0900

    feat: Create Email sending feature on contact page

--- src/components/Pages/Contact.tsx
+++ src/components/Pages/Contact.tsx
@@ -1,27 +1,94 @@
+import React, { useState } from 'react';
 import emailjs from '@emailjs/browser';
-import { useRef } from 'react';
+import styled from 'styled-components';
 
-const Contact = () => {
-  const form = useRef();
+const Container = styled.div`
+  height: calc(100vh - 3rem);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  >form {
+    padding: 2rem;
+    background-color: white;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    >input {
+      all: unset;
+    }
+    >textarea {
+      resize: none;
+      width: 20rem;
+      height: 20rem;
+      :focus {
+        outline: none;
+      }
+    }
+    >button {
+      all: unset;
+      cursor: pointer;
+      margin-left: 17.5rem;
+    }
+  }
+`
 
-  const sendEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+const Contact: React.FC = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const current:string = form.current as unknown as string
+    // Your EmailJS service ID, template ID, and user ID
+    const serviceId:string = process.env.REACT_APP_EMAILJS_SERVICE_ID as string;
+    const templateId:string = process.env.REACT_APP_EMAILJS_TEMPLATE_ID as string;
+    const userId:string = process.env.REACT_APP_EMAILJS_USER_ID as string;
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', current, 'YOUR_USER_ID')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
+    console.log(userId);
+    // Send the email
+    emailjs.send(serviceId, templateId, {
+      from_name: name,
+      from_email: email,
+      message: message,
+    }, userId)
+      .then(() => {
+        console.log('Email successfully sent!');
+        // Clear form fields
+        setName('');
+        setEmail('');
+        setMessage('');
+      })
+      .catch((error) => {
+        console.error('Error sending email:', error);
       });
+
   };
+
   return (
-    <>
-      <ul>
-      </ul>
-    </>
-  )
-}
+    <Container>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="Your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <textarea
+            placeholder="Your message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
+          <button type="submit">Send</button>
+      </form>
+    </Container>
+  );
+};
 
 export default Contact;
